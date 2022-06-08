39aae74c0fd160aa1f6f2e12a9876402d7cee268
sudhir <sudhir.oc11@gmail.com>
2022-06-08 02:36:09 -0700

    firstcommit

---
+++ src/components/crudeapp.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="container" style="max-width: 600px">
+    <h2 class="text-center mt-5">CRUDE APP</h2>
+
+    <div class="d-flex mt-5">
+      <input
+        type="text"
+        v-model="task"
+        placeholder="Enter task"
+        class="w-100 form-control"
+      />
+      <button class="btn btn-warning rounded-0" @click="submitTask">
+        SUBMIT
+      </button>
+    </div>
+
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col" style="width: 120px">Status</th>
+          <th scope="col" class="text-center">Delete</th>
+          <th scope="col" class="text-center">Edit</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>
+            <span :class="{ 'completed-task': task.status === 'finished' }">
+              {{ task.name }}
+            </span>
+          </td>
+          <td>
+            <span
+              class="pointer noselect"
+              @click="changeStatus(index)"
+              :class="{
+                'text-danger': task.status === 'devlopment',
+                'text-success': task.status === 'finished',
+                'text-warning': task.status === 'in-progress',
+              }"
+            >
+              {{ capitalizeFirstChar(task.status) }}
+            </span>
+          </td>
+          <td class="text-center">
+            <div @click="deleteTask(index)">
+              <span class="fa fa-trash pointer"></span>
+            </div>
+          </td>
+          <td class="text-center">
+            <div @click="editTask(index)">
+              <p class="fa fa-pen pointer"></p>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {  
+  mounted(){
+    if(localStorage.NEW_TASK_ADDED){
+      let isUsers = localStorage.NEW_TASK_ADDED
+      this.tasks = JSON.parse(isUsers)
+    }
+  },
+  data() {
+    return {
+      task: "",
+      editedTask: null,
+      statuses: ["devlopment", "in-progress", "finished"],
+      tasks: [
+        {
+          name: "Steal bananas from the supermarket.",
+          status: "devlopment",
+        },
+        {
+          name: "Eat 1 kg chocolate in 1 hour.",
+          status: "in-progress",
+        },
+       
+      ],
+    };
+  },
+  methods: {
+    capitalizeFirstChar(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+    changeStatus(index) {
+      let newIndex = this.statuses.indexOf(this.tasks[index].status);
+      if (++newIndex > 2) newIndex = 0;
+      this.tasks[index].status = this.statuses[newIndex];
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    editTask(index) {
+      this.task = this.tasks[index].name;
+      this.editedTask = index;
+    },
+    submitTask() {
+      if (this.task.length === 0) return;
+      if (this.editedTask != null) {
+        this.tasks[this.editedTask].name = this.task;
+        this.editedTask = null;
+      } else {
+        this.tasks.push({
+          name: this.task,
+          status: "todo",
+        });
+
+        localStorage.setItem("NEW_TASK_ADDED", JSON.stringify(this.tasks));
+      }
+      this.task = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}.completed-task {
+  text-decoration: line-through;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <crudeapp />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import crudeapp from '@/components/crudeapp.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    crudeapp
   }
 }
 </script>
